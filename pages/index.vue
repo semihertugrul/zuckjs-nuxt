b4460d1aca1d73370506fc44932b2910665562ff
Semih Ertuğrul <semihertugrul@hotmail.com>
2019-12-08 23:18:39 +0300

    update

--- pages/index.vue
+++ pages/index.vue
@@ -55,6 +55,7 @@ export default {
     return {
       storiesElement: null,
       storiesApi: null,
+      // storyData: [],
       storyData: [
         {
           id: "ramon",
@@ -64,7 +65,7 @@ export default {
           link: "",
           lastUpdated: 1575637794.095,
           category: "DÜNYA & POLİTİKA",
-          seen: true,
+          seen: false,
           items: [
             {
               id: "ramon-1",
@@ -115,7 +116,7 @@ export default {
           link: "",
           lastUpdated: 1575637794.095,
           category: "Ekonomi",
-          seen: true,
+          seen: false,
           items: [
             {
               id: "ramon-1",
@@ -158,6 +159,126 @@ export default {
             }
           ]
         }
+      ],
+      apiData: [
+        {
+          id: 1222,
+          siteId: 4,
+          mainCategory: {
+            id: 1,
+            name: "Gündem"
+          },
+          title: "Bu Sabah",
+          publishDate: "2019-11-20T18:56:20.713",
+          expirationDate: "2019-12-20T18:56:20.713",
+          createdDate: "2019-11-20T18:56:20.713",
+          updateDate: "2019-11-20T18:56:20.713",
+          user: {
+            id: 1,
+            name: "adem çınar"
+          },
+          status: 1,
+          coverPhotoPath:
+            "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/users/1.jpg",
+          stories: [
+            {
+              type: "photo",
+              user: {
+                id: 1,
+                name: "yusuf çınar"
+              },
+              url:
+                "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/1.jpg",
+              filePath:
+                "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/1.jpg",
+              createdDate: "2019-11-20T18:56:20.713",
+              isCoverPhoto: true
+            },
+            {
+              type: "video",
+              duration: "05:10",
+              user: {
+                id: 1,
+                name: "taha çınar"
+              },
+              url:
+                "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/2.mp4",
+              filePath:
+                "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/2.mp4",
+              createdDate: "2019-11-20T18:56:20.713"
+            }
+            // {
+            //   type: "text",
+            //   user: {
+            //     id: 1,
+            //     name: "bora çınar"
+            //   },
+            //   url:
+            //     "https://www.gzt.com/dunya-politika/arnavutluktaki-depremde-olu-ve-yarali-sayisi-artiyor-3514989",
+            //   content: "Deniz çok güzel sende gelsene",
+            //   createdDate: "2019-11-20T18:56:20.713"
+            // }
+          ]
+        }
+        // {
+        //   id: 1222,
+        //   siteId: 4,
+        //   mainCategory: {
+        //     id: 1,
+        //     name: "Gündem"
+        //   },
+        //   title: "Soçi 2019",
+        //   publishDate: "2019-11-20T18:56:20.713",
+        //   expirationDate: "2019-12-20T18:56:20.713",
+        //   createdDate: "2019-11-20T18:56:20.713",
+        //   updateDate: "2019-11-20T18:56:20.713",
+        //   user: {
+        //     id: 1,
+        //     name: "adem çınar"
+        //   },
+        //   status: 1,
+        //   coverPhotoPath:
+        //     "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/users/1.jpg",
+        //   stories: [
+        //     {
+        //       type: "photo",
+        //       user: {
+        //         id: 1,
+        //         name: "yusuf çınar"
+        //       },
+        //       url:
+        //         "https://www.gzt.com/dunya-politika/arnavutluktaki-depremde-olu-ve-yarali-sayisi-artiyor-3514989",
+        //       filePath:
+        //         "/resim/imagecrop/2019/11/26/04/57/resized_1e6a0-A865828_04.jpg",
+        //       createdDate: "2019-11-20T18:56:20.713",
+        //       isCoverPhoto: true
+        //     },
+        //     {
+        //       type: "video",
+        //       duration: "05:10",
+        //       user: {
+        //         id: 1,
+        //         name: "taha çınar"
+        //       },
+        //       url:
+        //         "https://www.gzt.com/dunya-politika/arnavutluktaki-depremde-olu-ve-yarali-sayisi-artiyor-3514989",
+        //       filePath:
+        //         "/resim/upload/video/2019/06/30/af68ef013fac4a1c9e031eee7ba03d52.mp4",
+        //       createdDate: "2019-11-20T18:56:20.713"
+        //     }
+        //     // {
+        //     //   type: "text",
+        //     //   user: {
+        //     //     id: 1,
+        //     //     name: "bora çınar"
+        //     //   },
+        //     //   url:
+        //     //     "https://www.gzt.com/dunya-politika/arnavutluktaki-depremde-olu-ve-yarali-sayisi-artiyor-3514989",
+        //     //   content: "Deniz çok güzel sende gelsene",
+        //     //   createdDate: "2019-11-20T18:56:20.713"
+        //     // }
+        //   ]
+        // }
       ]
     };
   },
@@ -168,82 +289,55 @@ export default {
         ? JSON.parse(window.localStorage[keyName])
         : false;
     },
-    transformData: function(data) {},
-    dateConvert: function(dateStr = "2019-11-20T18:56:20.713") {
-      return new Date(dateStr).valueOf();
+    dateConvert: function(dateStr) {
+      return new Date(dateStr).valueOf() / 1000;
+    },
+
+    expirationDateControl: function(expirationDate) {
+      return expirationDate ? new Date(expirationDate) > new Date() : true;
+    },
+
+    transformItemData: function(id, item, index) {
+      return {
+        id: String(id) + "-" + String(index),
+        type: item.type,
+        length: 6,
+        src: item.filePath,
+        preview: item.filePath,
+        link: item.url,
+        linkText: item.url ? "Detayı Gör" : false,
+        seen: false,
+        time: this.dateConvert(item.createdDate)
+      };
+    },
+    transformMainData: function(data) {
+      return {
+        id: String(data.id),
+        photo: data.coverPhotoPath,
+        name: data.title,
+        link: "",
+        lastUpdated: this.dateConvert(data.createdDate),
+        category: data.mainCategory.name,
+        seen: true,
+        items: data.stories.map((item, i) =>
+          this.transformItemData(data.id, item, i + 1)
+        )
+      };
+    },
+    showData: function() {
+      this.storyData = this.apiData
+        .filter(
+          x =>
+            x.status === 1 &&
+            x.siteId === 4 &&
+            this.expirationDateControl(x.expirationDate)
+        )
+        .map(data => this.transformMainData(data));
     }
+  },
+  beforeMount() {
+    // this.showData();
   }
-
-  // render(h) {
-  //   console.warn("semih render");
-  //   const timelineItems = [];
-
-  //   this.storyData.map((story, storyId) => {
-  //     const storyItems = [];
-
-  //     story.items.map(storyItem => {
-  //       storyItems.push(
-  //         <li
-  //           key={storyItem.id}
-  //           data-id={storyItem.id}
-  //           data-time={storyItem.time}
-  //           className={storyItem.seen ? "seen" : ""}
-  //         >
-  //           <a
-  //             href={storyItem.src}
-  //             data-type={storyItem.type}
-  //             data-length={storyItem.length}
-  //             data-link={storyItem.link}
-  //             data-linkText={storyItem.linkText}
-  //           >
-  //             <img src={storyItem.preview} />
-  //           </a>
-  //         </li>
-  //       );
-  //     });
-
-  //     let arrayFunc = story.seen ? "push" : "unshift";
-  //     timelineItems[arrayFunc](
-  //       <div
-  //         className={story.seen ? "story seen" : "story"}
-  //         key={storyId}
-  //         data-id={storyId}
-  //         data-last-updated={story.lastUpdated}
-  //         data-photo={story.photo}
-  //       >
-  //         <a className="item-link" href={story.link}>
-  //           <span className="item-preview">
-  //             <img src={story.photo} />
-  //           </span>
-  //           <span
-  //             className="info"
-  //             itemProp="author"
-  //             itemScope=""
-  //             itemType="http://schema.org/Person"
-  //           >
-  //             <strong className="name" itemProp="name">
-  //               {story.name}
-  //             </strong>
-  //             <span className="time">{story.lastUpdated}</span>
-  //           </span>
-  //         </a>
-
-  //         <ul className="items">{storyItems}</ul>
-  //       </div>
-  //     );
-  //   });
-
-  //   return h({
-  //     template: `<div>
-  //         <div
-  //           id="stories"
-  //           className="storiesWrapper"
-  //         >
-  //           ${timelineItems}
-  //         </div>
-  //       </div>`
-  //   });
-  // }
 };
 </script>
 
