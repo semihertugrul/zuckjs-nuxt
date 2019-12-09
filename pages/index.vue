88ffdd0f8af27242b2258183e27202774c61e88b
Semih Ertuğrul <semih.ertugrul@albs-MacBook-Pro-2.local>
2019-12-09 14:25:56 +0300

    update

--- pages/index.vue
+++ pages/index.vue
@@ -49,117 +49,15 @@
 </template>
 
 <script>
+import "~/plugins/Zuck.js";
 export default {
   name: "app",
   data: function() {
     return {
       storiesElement: null,
       storiesApi: null,
-      // storyData: [],
-      storyData: [
-        {
-          id: "ramon",
-          photo:
-            "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/users/1.jpg",
-          name: "Soçi 2019",
-          link: "",
-          lastUpdated: 1575637794.095,
-          category: "DÜNYA & POLİTİKA",
-          seen: false,
-          items: [
-            {
-              id: "ramon-1",
-              type: "photo",
-              length: 200,
-              src:
-                "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/1.jpg",
-              preview:
-                "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/1.jpg",
-              link: "",
-              linkText: false,
-              seen: false,
-              time: 1575042980.713
-            }
-            // {
-            //   id: "ramon-2",
-            //   type: "video",
-            //   length: 3,
-            //   src:
-            //     "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/2.mp4",
-            //   preview:
-            //     "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/2.jpg",
-            //   link: "",
-            //   linkText: false,
-            //   seen: false,
-            //   time: 1574956580.713
-            // },
-            // {
-            //   id: "ramon-3",
-            //   type: "photo",
-            //   length: 3,
-            //   src:
-            //     "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/3.png",
-            //   preview:
-            //     "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/3.png",
-            //   link: "",
-            //   linkText: false,
-            //   seen: false,
-            //   time: 1574956580.713
-            // }
-          ]
-        },
-        {
-          id: "semih",
-          photo:
-            "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/users/1.jpg",
-          name: "semih sffd",
-          link: "",
-          lastUpdated: 1575637794.095,
-          category: "Ekonomi",
-          seen: false,
-          items: [
-            {
-              id: "ramon-1",
-              type: "photo",
-              length: 200,
-              src:
-                "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/1.jpg",
-              preview:
-                "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/1.jpg",
-              link: "",
-              linkText: false,
-              seen: false,
-              time: 1575042980.713
-            },
-            {
-              id: "ramon-2",
-              type: "video",
-              length: 3,
-              src:
-                "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/2.mp4",
-              preview:
-                "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/2.jpg",
-              link: "",
-              linkText: false,
-              seen: false,
-              time: 1574956580.713
-            },
-            {
-              id: "ramon-3",
-              type: "photo",
-              length: 3,
-              src:
-                "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/3.png",
-              preview:
-                "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/3.png",
-              link: "",
-              linkText: false,
-              seen: false,
-              time: 1574956580.713
-            }
-          ]
-        }
-      ],
+      storyData: [],
+
       apiData: [
         {
           id: 1222,
@@ -188,7 +86,7 @@ export default {
                 name: "yusuf çınar"
               },
               url:
-                "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/1.jpg",
+                "https://resizer.piri.net/resize.aspx?w=593&h=0&url=/resim/imagecrop/2019/12/09/11/14/resized_8b996-c9a2bc73648ca169175d4743a2c02bb35a76b57c.jpg",
               filePath:
                 "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/1.jpg",
               createdDate: "2019-11-20T18:56:20.713",
@@ -202,93 +100,95 @@ export default {
                 name: "taha çınar"
               },
               url:
-                "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/2.mp4",
+                "https://beta.gzt.com/dunya-politika/turkiye-sinif-atladi-insani-gelismede-en-yuksek-kategoriye-yukseldi-3515759",
               filePath:
                 "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/2.mp4",
               createdDate: "2019-11-20T18:56:20.713"
             }
-            // {
-            //   type: "text",
-            //   user: {
-            //     id: 1,
-            //     name: "bora çınar"
-            //   },
-            //   url:
-            //     "https://www.gzt.com/dunya-politika/arnavutluktaki-depremde-olu-ve-yarali-sayisi-artiyor-3514989",
-            //   content: "Deniz çok güzel sende gelsene",
-            //   createdDate: "2019-11-20T18:56:20.713"
-            // }
+          ]
+        },
+        {
+          id: 1223,
+          siteId: 4,
+          mainCategory: {
+            id: 1,
+            name: "Ekonomi"
+          },
+          title: "Soçi 2019",
+          publishDate: "2019-11-20T18:56:20.713",
+          expirationDate: "2019-12-20T18:56:20.713",
+          createdDate: "2019-11-20T18:56:20.713",
+          updateDate: "2019-11-20T18:56:20.713",
+          user: {
+            id: 1,
+            name: "adem çınar"
+          },
+          status: 1,
+          coverPhotoPath:
+            "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/users/1.jpg",
+          stories: [
+            {
+              type: "photo",
+              user: {
+                id: 1,
+                name: "yusuf çınar"
+              },
+              url:
+                "https://www.gzt.com/dunya-politika/arnavutluktaki-depremde-olu-ve-yarali-sayisi-artiyor-3514989",
+              filePath:
+                "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/1.jpg",
+              createdDate: "2019-11-20T18:56:20.713",
+              isCoverPhoto: true
+            },
+            {
+              type: "video",
+              duration: "05:10",
+              user: {
+                id: 1,
+                name: "taha çınar"
+              },
+              url:
+                "https://www.gzt.com/dunya-politika/arnavutluktaki-depremde-olu-ve-yarali-sayisi-artiyor-3514989",
+              filePath:
+                "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/4.mp4",
+              createdDate: "2019-11-20T18:56:20.713"
+            }
           ]
         }
-        // {
-        //   id: 1222,
-        //   siteId: 4,
-        //   mainCategory: {
-        //     id: 1,
-        //     name: "Gündem"
-        //   },
-        //   title: "Soçi 2019",
-        //   publishDate: "2019-11-20T18:56:20.713",
-        //   expirationDate: "2019-12-20T18:56:20.713",
-        //   createdDate: "2019-11-20T18:56:20.713",
-        //   updateDate: "2019-11-20T18:56:20.713",
-        //   user: {
-        //     id: 1,
-        //     name: "adem çınar"
-        //   },
-        //   status: 1,
-        //   coverPhotoPath:
-        //     "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/users/1.jpg",
-        //   stories: [
-        //     {
-        //       type: "photo",
-        //       user: {
-        //         id: 1,
-        //         name: "yusuf çınar"
-        //       },
-        //       url:
-        //         "https://www.gzt.com/dunya-politika/arnavutluktaki-depremde-olu-ve-yarali-sayisi-artiyor-3514989",
-        //       filePath:
-        //         "/resim/imagecrop/2019/11/26/04/57/resized_1e6a0-A865828_04.jpg",
-        //       createdDate: "2019-11-20T18:56:20.713",
-        //       isCoverPhoto: true
-        //     },
-        //     {
-        //       type: "video",
-        //       duration: "05:10",
-        //       user: {
-        //         id: 1,
-        //         name: "taha çınar"
-        //       },
-        //       url:
-        //         "https://www.gzt.com/dunya-politika/arnavutluktaki-depremde-olu-ve-yarali-sayisi-artiyor-3514989",
-        //       filePath:
-        //         "/resim/upload/video/2019/06/30/af68ef013fac4a1c9e031eee7ba03d52.mp4",
-        //       createdDate: "2019-11-20T18:56:20.713"
-        //     }
-        //     // {
-        //     //   type: "text",
-        //     //   user: {
-        //     //     id: 1,
-        //     //     name: "bora çınar"
-        //     //   },
-        //     //   url:
-        //     //     "https://www.gzt.com/dunya-politika/arnavutluktaki-depremde-olu-ve-yarali-sayisi-artiyor-3514989",
-        //     //   content: "Deniz çok güzel sende gelsene",
-        //     //   createdDate: "2019-11-20T18:56:20.713"
-        //     // }
-        //   ]
-        // }
       ]
     };
   },
 
+  beforeMount() {
+    this.showData();
+  },
+
+  mounted() {
+    new Zuck("stories", {
+      skin: "snapgram", // container class
+      avatars: true, // shows user photo instead of last story item preview
+      paginationArrows: false,
+      list: false, // displays a timeline instead of carousel
+      cubeEffect: true, // enables the 3d cube effect when sliding story - may decrease performance
+      autoFullScreen: true, // enables fullscreen on mobile browsers
+      backButton: false,
+      backNative: true, // uses window history to enable back button on browsers/android
+      previousTap: true, // use 1/3 of the screen to navigate to previous item when tap the story
+      localStorage: true,
+      stories: []
+    });
+  },
+
   methods: {
     localStorageControl: function(id) {
-      return window.localStorage[`zuck-${id}-seenItems`]
-        ? JSON.parse(window.localStorage[keyName])
-        : false;
+      const keyName = `zuck-stories-seenItems`;
+      if (window.localStorage[keyName]) {
+        return !!JSON.parse(window.localStorage[keyName])[id];
+      } else {
+        return false;
+      }
     },
+
     dateConvert: function(dateStr) {
       return new Date(dateStr).valueOf() / 1000;
     },
@@ -310,6 +210,7 @@ export default {
         time: this.dateConvert(item.createdDate)
       };
     },
+
     transformMainData: function(data) {
       return {
         id: String(data.id),
@@ -318,14 +219,15 @@ export default {
         link: "",
         lastUpdated: this.dateConvert(data.createdDate),
         category: data.mainCategory.name,
-        seen: true,
+        seen: this.localStorageControl(data.id),
         items: data.stories.map((item, i) =>
           this.transformItemData(data.id, item, i + 1)
         )
       };
     },
+
     showData: function() {
-      this.storyData = this.apiData
+      let tmpData = this.apiData
         .filter(
           x =>
             x.status === 1 &&
@@ -333,12 +235,16 @@ export default {
             this.expirationDateControl(x.expirationDate)
         )
         .map(data => this.transformMainData(data));
+
+      this.storyData.push(...tmpData.filter(story => story.seen === false));
+      this.storyData.push(...tmpData.filter(story => story.seen === true));
     }
-  },
-  beforeMount() {
-    // this.showData();
   }
 };
 </script>
 
-<style></style>
+<style>
+@import "~/assets/css/snapgram.min.css";
+@import "~/assets/css/zuck.min.css";
+@import "~/assets/css/story.css";
+</style>
--- plugins/story.js
+++ plugins/story.js
@@ -1,10 +1,8 @@
 import Vue from "vue";
 
 // import Zuck from "zuck.js";
+
 import "~/plugins/Zuck.js";
-import "~/assets/css/snapgram.min.css";
-import "~/assets/css/zuck.min.css";
-import "~/assets/css/story.css";
 
 Vue.use(
   Zuck,
@@ -13,14 +11,117 @@ Vue.use(
     avatars: true, // shows user photo instead of last story item preview
     paginationArrows: false,
     list: false, // displays a timeline instead of carousel
-
     cubeEffect: true, // enables the 3d cube effect when sliding story - may decrease performance
     autoFullScreen: true, // enables fullscreen on mobile browsers
     backButton: false,
     backNative: true, // uses window history to enable back button on browsers/android
     previousTap: true, // use 1/3 of the screen to navigate to previous item when tap the story
     localStorage: true,
-
     stories: []
   })
 );
+
+// storyData: [
+//   {
+//     id: "ramon",
+//     photo:
+//       "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/users/1.jpg",
+//     name: "Soçi 2019",
+//     link: "",
+//     lastUpdated: 1575637794.095,
+//     category: "DÜNYA & POLİTİKA",
+//     seen: false,
+//     items: [
+//       {
+//         id: "ramon-1",
+//         type: "photo",
+//         length: 200,
+//         src:
+//           "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/1.jpg",
+//         preview:
+//           "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/1.jpg",
+//         link: "",
+//         linkText: false,
+//         seen: false,
+//         time: 1575042980.713
+//       }
+//       {
+//         id: "ramon-2",
+//         type: "video",
+//         length: 3,
+//         src:
+//           "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/2.mp4",
+//         preview:
+//           "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/2.jpg",
+//         link: "",
+//         linkText: false,
+//         seen: false,
+//         time: 1574956580.713
+//       },
+//       {
+//         id: "ramon-3",
+//         type: "photo",
+//         length: 3,
+//         src:
+//           "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/3.png",
+//         preview:
+//           "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/3.png",
+//         link: "",
+//         linkText: false,
+//         seen: false,
+//         time: 1574956580.713
+//       }
+//     ]
+//   },
+//   {
+//     id: "semih",
+//     photo:
+//       "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/users/1.jpg",
+//     name: "semih sffd",
+//     link: "",
+//     lastUpdated: 1575637794.095,
+//     category: "Ekonomi",
+//     seen: false,
+//     items: [
+//       {
+//         id: "ramon-1",
+//         type: "photo",
+//         length: 200,
+//         src:
+//           "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/1.jpg",
+//         preview:
+//           "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/1.jpg",
+//         link: "",
+//         linkText: false,
+//         seen: false,
+//         time: 1575042980.713
+//       },
+//       {
+//         id: "ramon-2",
+//         type: "video",
+//         length: 3,
+//         src:
+//           "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/2.mp4",
+//         preview:
+//           "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/2.jpg",
+//         link: "",
+//         linkText: false,
+//         seen: false,
+//         time: 1574956580.713
+//       },
+//       {
+//         id: "ramon-3",
+//         type: "photo",
+//         length: 3,
+//         src:
+//           "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/3.png",
+//         preview:
+//           "https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/3.png",
+//         link: "",
+//         linkText: false,
+//         seen: false,
+//         time: 1574956580.713
+//       }
+//     ]
+//   }
+// ],
